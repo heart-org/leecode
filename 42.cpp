29d56895edfc706ba944b087193b04c7fb2dd734
orange <org159753@163.com>
2023-11-10 21:44:06 +0800

    leetcode 42提交

--- 42.cpp
+++ 42.cpp
@@ -13,6 +13,33 @@
 
 using namespace std;
 
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        stack<int> s;
+        int res = 0;
+        int h = 0;
+        int width = 0;
+        int left = 0;
+        for (int i = 0; i < height.size(); ++i) {
+            if (!s.empty() && height[s.top()] < height[i]){
+                while (!s.empty() && height[s.top()] < height[i]){
+                    int mid = s.top();
+                    s.pop();
+                    if (!s.empty()){
+                        left = s.top();
+                        h = min(height[left], height[i]) - height[mid];
+                        width = i - left - 1;
+                        res += h * width;
+                    }
+                }
+            }
+            s.push(i);
+        }
+        return res;
+    }
+};
+
 int main(){
 
     return 0;
