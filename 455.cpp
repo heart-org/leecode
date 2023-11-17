19b3a1f45c44dd46b5e26dd9691929c8b909f61f
orange <org159753@163.com>
2023-11-17 20:40:50 +0800

    leetcode 455提交----贪心

---
+++ 455.cpp
@@ -0,0 +1,40 @@
+#include "iostream"
+#include "queue"
+#include "stack"
+#include "vector"
+#include "algorithm"
+#include "cstring"
+#include "cmath"
+#include "set"
+#include "map"
+#include "numeric"
+#include "unordered_set"
+#include "unordered_map"
+#include "array"
+#include "cctype"
+#include "list"
+
+using namespace std;
+
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+        int index = s.size() - 1;
+        int res = 0;
+        for (int i = g.size() - 1; i >= 0 ; --i) {
+            while(index >= 0 && s[index] >= g[i]){
+                res++;
+                index--;
+                break;
+            }
+        }
+        return res;
+    }
+};
+
+int main(){
+
+    return 0;
+}
