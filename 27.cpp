432adfeaeb39050553340e6714ee0c5110e86740
orange <org159753@163.com>
2023-11-18 11:38:12 +0800

    leetcode 27update

--- 27.cpp
+++ 27.cpp
@@ -25,6 +25,16 @@ public:
     }
 };
 
+/*
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        nums.erase(std::remove(nums.begin(), nums.end(), val), nums.end());
+        return nums.size();
+    }
+};
+*/
+
 int main(){
 
     return 0;
