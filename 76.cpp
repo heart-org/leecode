d1b6e97b40cdd7a7dcdbc96e050dc65cb23d5c45
orange <org159753@163.com>
2023-12-07 21:46:32 +0800

    update

--- 76.cpp
+++ 76.cpp
@@ -16,6 +16,57 @@
 
 using namespace std;
 
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        int t_hash[26];
+        int s_hash[26];
+        string res;
+        //存所有t的hash
+        for (int i = 0; i < t.length(); ++i) {
+            t_hash[t[i] - 'A']++;
+            s_hash[s[i] - 'A']++;
+        }
+        int slow, fast;
+        slow = 0;
+        fast = t.length();
+        int flag = true;
+        int sum = INT_MAX;
+        while (fast < t.length()) {
+            for (auto i: s_hash) {
+                if (t_hash[i] == 0 || t_hash[i] == s_hash[i])
+                    continue;
+                else {
+                    flag = false;//不满足继续增大窗口
+                    break;
+                }
+            }
+            if (!flag) {
+                s_hash[s[fast++] - 'A']++;
+            } else {
+                sum = min(sum, fast - slow);
+                s_hash[s[slow++] - 'A']--;
+                s_hash[s[slow] - 'A']++;
+            }
+            flag = true;
+        }
+        while (fast - slow > t.length()) {
+            s_hash[s[slow++] - 'A']--;
+            s_hash[s[slow] - 'A']++;
+            for (auto i: s_hash) {
+                if (t_hash[i] == 0 || t_hash[i] == s_hash[i])
+                    continue;
+                else {
+                    return sum;
+                }
+            }
+            sum = min(sum, fast - slow);
+        }
+
+
+    }
+};
+
 int main() {
 
     return 0;
