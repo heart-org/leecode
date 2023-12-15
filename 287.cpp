538b7cd3ff2f3ce439d271589710aca64203f5ad
orange <org159753@163.com>
2023-12-15 14:20:50 +0800

    update

---
+++ 287.cpp
@@ -0,0 +1,46 @@
+#include "iostream"
+#include "queue"
+#include "stack"
+#include "vector"
+#include "algorithm"
+#include "cstring"
+#include "cmath"
+#include "set"
+#include "map"
+#include "numeric"
+#include "unordered_set"
+#include "unordered_map"
+#include "array"
+#include "cctype"
+#include "list"
+
+using namespace std;
+
+class Solution {
+public:
+    int findDuplicate(vector<int> &nums) {
+        if (nums.size() == 2) {
+            return nums[0];
+        }
+        int slow = nums[0];
+        int fast = nums[nums[0]];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+        slow = 0;
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+};
+
+int main() {
+    Solution s;
+    vector<int> nums = {3, 1, 3, 4, 2};
+    s.findDuplicate(nums);
+
+    return 0;
+}
