363a3ed8951ec212b3db182b1f4ca98a877cdfb4
orange <org159753@163.com>
2024-01-07 17:55:39 +0800

    update

--- 100.cpp
+++ 100.cpp
@@ -16,6 +16,45 @@
 
 using namespace std;
 
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+using vecType = vector<int>;
+
+class Solution {
+public:
+    bool isSameTree(TreeNode *p, TreeNode *q) {
+        preOrder(p, v1);
+        preOrder(q, v2);
+        if (v1 == v2)
+            return true;
+        return false;
+    }
+
+private:
+    vecType v1;
+    vecType v2;
+
+    void preOrder(TreeNode *Node, vecType &vec) {
+        if (Node == nullptr) {
+            vec.emplace_back(INT_MAX);
+            return;
+        }
+        vec.emplace_back(Node->val);
+        preOrder(Node->left, vec);
+        preOrder(Node->right, vec);
+    }
+};
+
 int main() {
 
 
