d03fc525a5ec8f58e0abb084326ee761e895104a
orange <org159753@163.com>
2024-01-07 19:41:19 +0800

    update

---
+++ 222.cpp
@@ -0,0 +1,44 @@
+#include "iostream"
+#include "queue"
+#include "stack"
+#include "vector"
+#include "algorithm"
+#include "cstring"
+#include "cmath"
+#include "set"
+#include "map"
+#include "numeric"
+#include "unordered_set"
+#include "unordered_map"
+#include "array"
+#include "cctype"
+#include "list"
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int countNodes(TreeNode *root) {
+        if (root == nullptr)
+            return 0;
+        return 1 + countNodes(root->left) + countNodes(root->right);
+    }
+};
+
+int main() {
+    Solution s;
+
+    return 0;
+}
