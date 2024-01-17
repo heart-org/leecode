179a544b055d693f64467aa1a2fdb6aab3586fe7
orange <org159753@163.com>
2024-01-17 12:42:18 +0800

    update

---
+++ 700.cpp
@@ -0,0 +1,45 @@
+#include "iostream"
+#include "queue"
+#include "stack"
+#include "vector"
+#include "algorithm"
+#include "cstring"
+#include "cmath"
+#include "set"
+#include "map"
+#include "numeric"
+#include "unordered_set"
+#include "unordered_map"
+#include "array"
+#include "cctype"
+#include "list"
+
+using namespace std;
+
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    TreeNode *searchBST(TreeNode *root, int val) {
+        if (root == nullptr || root->val == val)
+            return root;
+        return searchBST((root->val < val ? root->right : root->left), val);
+    }
+};
+
+int main() {
+    Solution s;
+
+    return 0;
+}
