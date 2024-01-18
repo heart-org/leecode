df014bff5da2401abedfd5fca46b9e4fad2bc526
orange <org159753@163.com>
2024-01-18 14:53:34 +0800

    update

---
+++ 236.cpp
@@ -0,0 +1,54 @@
+#include "iostream"
+#include "queue"
+#include "stack"
+#include "vector"
+#include "algorithm"
+#include "cstring"
+#include "cmath"
+#include "set"
+#include "map"
+#include "numeric"
+#include "unordered_set"
+#include "unordered_map"
+#include "array"
+#include "cctype"
+#include "list"
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q) {
+        if (root == nullptr)
+            return root;
+        if (root == p || root == q)
+            return root;
+        TreeNode *left = lowestCommonAncestor(root->left, p, q);
+        TreeNode *right = lowestCommonAncestor(root->right, p, q);
+        if (left != nullptr && right != nullptr)
+            return root;
+        if (left == nullptr && right != nullptr)
+            return right;
+        if (left != nullptr && right == nullptr)
+            return left;
+        return nullptr;
+    }
+};
+
+int main() {
+    Solution s;
+
+    return 0;
+}
