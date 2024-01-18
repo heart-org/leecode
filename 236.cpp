141e43ac02aa02aec4da5ed7dd5e5f1bd5035b22
orange <org159753@163.com>
2024-01-18 14:55:51 +0800

    update

--- 236.cpp
+++ 236.cpp
@@ -28,7 +28,7 @@ struct TreeNode {
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
-class Solution {
+class Solution1 {
 public:
     TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q) {
         if (root == nullptr)
@@ -47,6 +47,21 @@ public:
     }
 };
 
+//精简版
+class Solution {
+public:
+    TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q) {
+        if (root == nullptr || root == p || root == q)
+            return root;
+        TreeNode *left = lowestCommonAncestor(root->left, p, q);
+        TreeNode *right = lowestCommonAncestor(root->right, p, q);
+        if (left != nullptr && right != nullptr)
+            return root;
+        if (left == nullptr)
+            return right;
+        return left;
+    }
+};
 int main() {
     Solution s;
 
