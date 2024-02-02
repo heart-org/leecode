03024dd3ea32c3d76d238b5e92bdba0c8419fae5
orange <org159753@163.com>
2024-02-02 14:24:32 +0800

    update

--- 455.cpp
+++ 455.cpp
@@ -16,6 +16,7 @@
 
 using namespace std;
 
+//while
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
@@ -34,6 +35,23 @@ public:
     }
 };
 
+//if
+class Solution1 {
+public:
+    int findContentChildren(vector<int> &g, vector<int> &s) {
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+        int index = s.size() - 1;
+        int res = 0;
+        for (int i = g.size() - 1; i >= 0; --i) {
+            if (index >= 0 && s[index] >= g[i]) {
+                res++;
+                index--;
+            }
+        }
+        return res;
+    }
+};
 int main(){
 
     return 0;
