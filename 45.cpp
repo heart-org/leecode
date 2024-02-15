7e2a10145f5d5f96eab2a825b78322a27da80aae
orange <org159753@163.com>
2024-02-15 14:42:45 +0800

    update

--- 45.cpp
+++ 45.cpp
@@ -24,7 +24,7 @@ public:
         int cover = 0;
         int step = 0;
         int first_cover = 0;
-        for (int i = 0; i <= cover; ++i) {
+        for (int i = 0; i < nums.size(); ++i) {
             cover = max(nums[i] + i, cover);
             if (i == first_cover) {
                 step++;
@@ -36,6 +36,25 @@ public:
         return step;
     }
 };
+
+//贪心
+int jump(vector<int> &nums) {
+    int step = 0;
+    int cur_cover = 0;
+    int next_cover = 0;
+    for (int i = 0; i < nums.size() - 1; ++i) {
+        next_cover = max(next_cover, i + nums[i]);//当前所能到达的最远距离
+        if (i == cur_cover) {
+            /*
+            倒数第二个为当前最远距离时，需要再次++，才能到达最后一个元素。
+            相反，如果到达倒数第二个元素还没有到达当前最远距离时，说明不需要再走一步就能够到达最远距离了。
+            */
+            step++;
+            cur_cover = next_cover;
+        }
+    }
+    return step;
+}
 
 int main() {
     Solution s;
