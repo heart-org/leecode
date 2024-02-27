d24faf98b5291a564bfdb70bf79ebf4fae1c0d56
orange <org159753@163.com>
2024-02-27 12:45:56 +0800

    update

---
+++ 62.cpp
@@ -0,0 +1,42 @@
+#include "iostream"
+#include "queue"
+#include "stack"
+#include "vector"
+#include "algorithm"
+#include "cstring"
+#include "cmath"
+#include "set"
+#include "map"
+#include "numeric"
+#include "unordered_set"
+#include "unordered_map"
+#include "array"
+#include "cctype"
+#include "list"
+
+using namespace std;
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m, vector<int>(n));
+        for (int i = 0; i < m; ++i) {
+            dp[i][0] = 1;
+        }
+        for (int i = 0; i < n; ++i) {
+            dp[0][i] = 1;
+        }
+        for (int i = 1; i < m; ++i) {
+            for (int j = 1; j < n; ++j) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+};
+
+int main() {
+    Solution s;
+
+    return 0;
+}
